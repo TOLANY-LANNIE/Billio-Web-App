0b044f884a9a6a9f11156237c77fcc0d2c37a20f
Thulani Mpofu <37704449+TOLANY-LANNIE@users.noreply.github.com>
2024-04-14 16:12:46 +0200

    Thulani|1958|updated the styling

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl,
+    password: new FormControl
+  });
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {}
 }
