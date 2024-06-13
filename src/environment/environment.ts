484e943e20a4a573e0de512c8d236ed96229a302
Thulani <tolanym_2@live.com>
2024-06-13 22:31:01 +0200

    updated the event details modal view

---
+++ src/environment/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    clientID: '665872029748-1d8gbrdhbp6nocnkg0mvq42h6uukao1l.apps.googleusercontent.com',
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyAjJ3P9I49IXGTvQdciUUP-ZG8j1banEJ4",
+      authDomain: "billio-c1cc5.firebaseapp.com",
+      projectId: "billio-c1cc5",
+      storageBucket: "billio-c1cc5.appspot.com",
+      messagingSenderId: "633454765191",
+      appId: "1:633454765191:web:20a3bd6440b1ff31d4c422",
+      measurementId: "G-L6PXV6TBPH"
+      },
+}
+
